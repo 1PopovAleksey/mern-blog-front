ab389cd380892615063ce2e4d0f9e9c2335bcf0f
Aleksey Popov <alekseypopov.dev@gmail.com>
2022-07-31 10:31:14 +0700

    Added react-router

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from "react";
+import {BrowserRouter} from "react-router-dom";
 import ReactDOM from "react-dom/client";
+import {Provider} from "react-redux";
+import store from "./redux/store";
 import App from "./App";
 import CssBaseline from "@mui/material/CssBaseline";
 
@@ -10,10 +13,14 @@ import { theme } from "./theme";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <React.StrictMode>
+  <>
     <CssBaseline />
     <ThemeProvider theme={theme}>
-      <App />
+      <BrowserRouter>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </BrowserRouter>
     </ThemeProvider>
-  </React.StrictMode>
+  </>
 );
